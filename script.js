03570c1bc2bb153b3b50cdf0b81db85f971492c5
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-06-28 06:57:16 -0400

    Writing the start btn functions

--- script.js
+++ script.js
@@ -5,5 +5,25 @@ var answerBtn = document.getElementById("answerBtn")
 var btnGrid = document.querySelector("btnGrid")
 var btn = document.querySelector("btn")
 var controls = document.querySelector("controls")
-var startBtn = document.getElementById("startBtn")
 var nextBtn = document.getElementById("nextBtn")
+
+
+var question = [
+    {
+        id: 1,
+        answers: {
+            filler: "future string"  
+        },
+        correctAnswer: "correct", // if it matches the chosen answer, then you move forward 
+    }
+]
+
+
+function startQuiz() {
+    var startBtn = document.getElementById("startBtn");
+    startBtn.addEventListener('click', (event) => {
+        var usersAnswer = document.querySelectorAll('btn')
+        console.log(startBtn, usersAnswer)
+    }
+    )
+}
