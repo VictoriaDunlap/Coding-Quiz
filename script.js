0aee1380686e7adbd267e25ef7391e6fbe80fccc
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-06-29 07:57:03 -0400

    Added question objects/arrays. Added question randomizer function. Started function to set net question

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
 var container = document.querySelector(".container")
 var questionContainer = document.getElementById("questionContainer")
-
-var answerBtn = document.getElementById("answerBtn")
+var startBtn = document.getElementById("startBtn");
+var answerBtns = document.getElementById("answerBtn")
 var btnGrid = document.querySelector("btnGrid")
 var btn = document.querySelector("btn")
 var controls = document.querySelector("controls")
 // var nextBtn = document.getElementById("nextBtn")
 var timerElem = document.querySelector("timerElem")
-
+var questionId = document.getElementById("question")
 // question.hidden();
 
+var shuffledQuestions, currentQuestionIndex
+
 function hideQuestion() {
-    var question = document.getElementById("question");
-    if (question.style.display === "none") {
-        question.style.display = "block";
+    // var question = document.getElementById("question");
+    if (questionToAnswer.style.display === "none") {
+        questionToAnswer.style.display = "block";
     } else {
-        question.style.display = "none";
+        questionToAnswer.style.display = "none";
     }
   }
 
-var questionToAnswer = [ // object to circulate through q's/a's
+var questionToAnswer = [ // array to circulate through q's/a's
     {
-        id: 'What are the desired results in a program?',
-        answers: {
-            filler: "output, input, function, display",   
-        },
-        correctAnswer: "output", // if it matches the chosen answer, then you move forward 
+        questiion: 'What are the desired results in a program?',
+        answers: [
+            {text: 'output', correct:true},
+            {text: 'input', correct:false},
+            {text: 'display', correct:false},
+            {text: 'function', correct:false}
+        ],
+        // correctAnswer: "output", // if it matches the chosen answer, then you move forward 
     }
 ]
 
+startBtn.addEventListener('click', startQuiz);
 
 function startQuiz() { // starts quiz 
-    var startBtn = document.getElementById("startBtn"); // var that grabs the id 
-    startBtn.addEventListener('click', (event) => { // event for onclick to start quiz
-        var usersAnswer = document.querySelectorAll('btn'); // var that grabs whatever btn the user chooses
-        console.log(startBtn, usersAnswer);
-
+    startBtn.classList.add('hidden');
+    shuffledQuestions = questionToAnswer.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainer.classList.remove('hidden');
+    setNextQuestion();
+    // var startBtn = document.getElementById("startBtn"); // var that grabs the id 
+    // startBtn.addEventListener('click', (event) => { // event for onclick to start quiz
+    //     var usersAnswer = document.querySelectorAll('btn'); // var that grabs whatever btn the user chooses
+    //     console.log(startBtn, usersAnswer);
+        console.log("Started");
         // i think i need a function that brings in the object and sifts through the answer that is chosen to compare to the correct answer
         // if answer chosen is correct it moves to another question and the page logs a point 
         // if answer chosen is wrong it moves to another question and subtracts 15 seconds from the timer 
     }
-    )
+//     )
+// }
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(questionToAnswer) {
+    questionId.innerText = questionToAnswer.questionToAnswer
+}
+
+function selectAnswer(){
+
 }
 
 function timer() { // timer element function 
@@ -61,3 +84,9 @@ function sendMessage() { // message that has people add initials and send them t
     input.setAttribute("type", "text");
     document.body.appendChild(input);
 }
+
+// Commonly used data types DO NOT include... strings, booleans, alerts, numbers
+// The condition of an if/else statement is enclosed within... quotes, curly brackets, parantheses, square brackets
+// Arrays in Javascript can be used to store... numbers and strings, other arrays, booleans, all of the above
+// String values must be enclosed within ____ when being assigned to variables. commas, curly brackets, quotes, parantheses
+// A very useful tool during development and debugging printing content to the debugger... Javascript, Terminal, onsole log, for loops  
